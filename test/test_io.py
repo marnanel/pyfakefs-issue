1f5dd6b213d023bc84eb10de2c2da4e281f94265
Marnanel Thurman <marnanel@thurman.org.uk>
2022-08-30 22:22:02 +0100

    Added example of workaround

--- test/test_io.py
+++ test/test_io.py
@@ -8,3 +8,13 @@ def test_inputstream(fs):
 
     stream = wombat.io.InputStream(FILENAME)
     assert stream.read()=='Capybaras\n'
+
+def test_inputstream_with_workaround(fs):
+
+    open.__self__.skip_names.remove('io')
+
+    with open(FILENAME, 'w') as f:
+        f.write('Capybaras\n')
+
+    stream = wombat.io.InputStream(FILENAME)
+    assert stream.read()=='Capybaras\n'
